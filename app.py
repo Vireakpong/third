5922df5153f05de0ca28e051fba50b50c557e683
unknown <vireakpong0257@gmail.com>
2020-09-21 17:17:42 +0700

    Second commit

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
 #\app.py
+import os
 from bottle import route, run
 
 @route('/')
 def mainn():
     return "Hello world"
 
-run (host = 'localhost', port=9000, debug = True, reloader = True)
+if 'DYNO' in os.environ:
+    run(host = '0.0.0.0', port=os.environ.get('PORT', 9000))
+else :
+    run (host = 'localhost', port=9000, debug = True, reloader = True)
